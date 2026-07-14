6d2b23f6b4aa0952c313e881e2fcde19b653c62f
Anna Kovalevskaia <131610551+anna-kovalevskaia@users.noreply.github.com>
2026-07-15 01:16:38 +0300

    fixed (#11)

--- dags_utils/operations/steamspy_all_ops.py
+++ dags_utils/operations/steamspy_all_ops.py
@@ -1,8 +1,8 @@
 import logging
-from typing import Optional, Any
+from typing import Optional
 
 import polars as pl
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from pathlib import Path
 
@@ -11,7 +11,15 @@ from dags_utils.sources.steamspy import SteamSpyClient
 logger = logging.getLogger(__name__)
 
 
+class IterArguments(BaseModel):
+    max_pages: int
+    stop_after_empty_pages: int
+    delay_seconds: float
+
+
+
 class SteamSpyAllModel(BaseModel):
+    model_config = ConfigDict(extra="ignore")
 
     appid: int
     name: str
@@ -32,63 +40,31 @@ class SteamSpyAllModel(BaseModel):
     ccu: Optional[int] = None
 
 
-def _steamspy_all_parse_json(data: dict[str, dict[str, Any]]) -> list[dict[str, Any]]:
-    """Parse JSON response from SteamSpy API."""
-    parse_data = [
-        {
-            "appid": record["appid"],
-            "name": record["name"],
-            "developer": record.get("developer"),
-            "publisher": record.get("publisher"),
-            "score_rank": record.get("score_rank"),
-            "positive": record.get("positive"),
-            "negative": record.get("negative"),
-            "userscore": record.get("userscore"),
-            "owners": record.get("owners"),
-            "average_forever": record.get("average_forever"),
-            "average_2weeks": record.get("average_2weeks"),
-            "median_forever": record.get("median_forever"),
-            "median_2weeks": record.get("median_2weeks"),
-            "price": record.get("price"),
-            "initialprice": record.get("initialprice"),
-            "discount": record.get("discount"),
-            "ccu": record.get("ccu")
-        }
-        for record in data.values()
-    ]
-
-    return parse_data
-
-
-def _steamspy_write_to_tmp(data: list[SteamSpyAllModel], full_file_path: str):
+def _steamspy_write_to_tmp(data: list[SteamSpyAllModel], full_file_path: Path):
     """Write SteamSpy data to a temporary file."""
     models = [row.model_dump() for row in data]
     df = pl.DataFrame(models)
     df.write_parquet(full_file_path)
-
-    logger.info("SteamSpy written %s", len(df))
 
 
 def steamspy_all_extract_to_tmp(
         client: SteamSpyClient,
         run_id: str,
         file_path: str,
-        max_pages: int,
-        allowed_empty_page: int,
-        delay_seconds: int
+        **kwargs
     ) -> str:
     """Process SteamSpy API data."""
+    parent_path = Path(file_path)/run_id
+    parent_path.mkdir(parents=True, exist_ok=True)
   
-    for page_num, page_data in client.steamspy_iter_all(max_pages=max_pages, stop_after_empty_pages=allowed_empty_page, delay_seconds=delay_seconds):
-        parse_result = _steamspy_all_parse_json(page_data)
-        logger.info("SteamSpy parsed %s", page_num)
+    for page_num, page_data in client.steamspy_iter_all(IterArguments(**kwargs)):
 
-        type_validate_result = [SteamSpyAllModel(**row) for row in parse_result]
-        logger.info("SteamSpy validated %s", page_num)
+        validate_result = [SteamSpyAllModel(**row) for row in page_data.values()]
+        logger.info("SteamSpy validated page=%s records=%s", page_num, len(validate_result))
 
-        parent_path = f"{file_path}/{run_id}"
-        Path(parent_path).mkdir(parents=True, exist_ok=True)
-        full_file_path = f"{parent_path}/page_{page_num}.parquet"
-        _steamspy_write_to_tmp(type_validate_result, full_file_path)
+        full_file_path = parent_path/f"page_{page_num}.parquet"
+
+        _steamspy_write_to_tmp(validate_result, full_file_path)
         logger.info("SteamSpy written %s", page_num)
-    return parent_path
+
+    return str(parent_path)
