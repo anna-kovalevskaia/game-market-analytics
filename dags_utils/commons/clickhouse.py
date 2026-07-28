d7d5f9a8fd9784e9e49528736b17cfed09e861ab
anna <kovalevskaiaanna83@gmail.com>
2026-07-28 15:51:44 +0300

    fixed

--- dags_utils/commons/clickhouse.py
+++ dags_utils/commons/clickhouse.py
@@ -42,7 +42,9 @@ class ClickHouseClient:
         partition_by: str = "toStartOfMonth(last_update)",
     ) -> str:
 
-        cols_sql = ",\n    ".join([f"{name} {clickhouse_type}" for name, clickhouse_type in columns])
+        cols_sql = (
+            ",\n    ".join([f"{name} {clickhouse_type}" for name, clickhouse_type in columns])
+        )
         cols_sql += (
             ",\n    row_hash UInt64 MATERIALIZED cityHash64("
             + ",".join([name for name, _ in columns])
